323782c142d16ecd9ba59225ad0b286403533d15
VidadiJavadov <vidadi4000@gmail.com>
2025-08-06 16:17:10 +0400

    3-flip_me_over.py

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Trnspose"""
+def matrix_transpose(matrix):
+    """function part"""
+    rows = len(matrix[0])
+    columns = len(matrix)
+    result = []
+    for i in range(rows):
+        new_row = []
+        for j in range(columns):
+            new_row.append(matrix[j][i])
+        result.append(new_row)
+
+    return result
