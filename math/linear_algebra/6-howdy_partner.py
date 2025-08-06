37a6f8f864db7906f1eb34fcb14dfbd6804fccea
VidadiJavadov <vidadi4000@gmail.com>
2025-08-06 21:43:07 +0400

    6-howdy_partner.py

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""concatenates arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    """function which returns cat_arrays"""
+    res = []
+    for item in arr1:
+        res.append(item)
+
+    for item in arr2:
+        res.append(item)
+
+    return res
