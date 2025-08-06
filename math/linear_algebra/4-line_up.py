ec88128436e27922d3e39a3290f5eff0e51cea43
VidadiJavadov <vidadi4000@gmail.com>
2025-08-06 16:35:28 +0400

    4-line_up.py

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""adding matrices"""
+
+
+def matrix_shape(matrix):
+    """function for shape"""
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        if len(matrix) == 0:
+            break
+        matrix = matrix[0]
+    return shape
+
+def add_arrays(arr1, arr2):
+    """function for adding matrices with same shape"""
+    result = []
+    if matrix_shape(arr1) == matrix_shape(arr2):
+        for i, j in zip(arr1, arr2):
+                result.append(i+j)
+        return result
+    else:
+        return None
