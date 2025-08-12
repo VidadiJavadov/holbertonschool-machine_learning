71a9c9c7b67d80f79df5feb4da65c4d83c0139af
VidadiJavadov <vidadi4000@gmail.com>
2025-08-12 16:58:43 +0400

    Update 10-matisse.py

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -4,17 +4,19 @@
 
 def poly_derivative(poly):
     """function that find that derivative"""
-    if isinstance(poly[0], int):
-        res = []
-        for i in range(1, len(poly)):
-            res.append(i * poly[i])
+    if (not isinstance(poly, list) or len(poly) == 0
+            or not all(isinstance(c, (float, int)) for c in poly)):
+        return None
 
-        res2 = []
-        for i in res:
-            if i == 0:
-                res2.append(i)
+    res = []
+    for i in range(1, len(poly)):
+        res.append(i * poly[i])
 
-        if res == res2:
-            return [0]
-        return res
-    return None
+    res2 = []
+    for i in res:
+        if i == 0:
+            res2.append(i)
+
+    if res == res2:
+        return [0]
+    return res
