52d0c3ce97d3b4fdb9fc6f3ec2bbc75dba29a07c
VidadiJavadov <vidadi4000@gmail.com>
2025-08-12 16:07:59 +0400

    Update 9-sum_total.py

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+"""Sum of i squared"""
+
+
 def summation_i_squared(n):
     """summation i squared"""
-    if not isinstance(n, int) and n < 1:
+    if not isinstance(n, int):
         return None
     return int(n*(n+1)*(2*n+1)/6)
