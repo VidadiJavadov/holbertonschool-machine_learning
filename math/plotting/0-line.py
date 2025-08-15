7284fc999f06ffb178b544776543d6b05291c8c2
VidadiJavadov <vidadi4000@gmail.com>
2025-08-15 20:46:51 +0400

    Update 0-line.py

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
+"""cubic grapgh"""
 
 def line():
-
+    """function cslculate graph"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-
     x = np.arange(0, 11)
-    plt.plot(y, color="red")
+
+    plt.plot(x, y, color="red")
     plt.show()
