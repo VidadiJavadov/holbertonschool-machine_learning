b7f8fed438671be5ebe50ae02ed16d733aadc6af
VidadiJavadov <vidadi4000@gmail.com>
2025-08-16 18:04:38 +0400

    4-frequency.py

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""Frequency"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """frequency"""
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor="black")
+    plt.title("Project A")
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+    plt.show()
