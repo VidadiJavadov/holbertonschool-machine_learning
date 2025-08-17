aca65440f5a476deb613db7b993f275e1443a112
VidadiJavadov <vidadi4000@gmail.com>
2025-08-17 05:12:20 +0400

    6-bars.py

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 def bars():
     """stacked bar cart Farrah, Fred, Felicia"""
     np.random.seed(5)
-    fruit = np.random.randint(0, 20, (4,3))
+    fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
     people = ["Farrah", "Fred", "Felicia"]
     x = np.arange(len(people))
@@ -22,10 +22,11 @@ def bars():
     plt.bar(people, apples, width, color="red")
     plt.bar(people, bananas, width, bottom=apples, color="yellow")
     plt.bar(people, oranges, width, bottom=apples+bananas, color="#ff8000")
-    plt.bar(people, peaches, width, 
+    plt.bar(people, peaches, width,
             bottom=apples+bananas+oranges, color="#ffe5b4")
     plt.legend(["apples", "bananas", "oranges", "peaches"])
     plt.ylim(0, 80)
     plt.ylabel("Quantity of Fruit")
     plt.title("Number of Fruit per Person")
     plt.show()
+    
