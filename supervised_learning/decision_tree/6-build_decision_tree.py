d843acf7c1d3f2f19a3a970a28460caec6524dc7
VidadiJavadov <vidadi4000@gmail.com>
2025-08-24 07:02:34 +0400

    6-build_decision_tree.py

--- supervised_learning/decision_tree/6-build_decision_tree.py
+++ supervised_learning/decision_tree/6-build_decision_tree.py
@@ -115,9 +115,9 @@ class Node:
             axis=0,
         )
 
-    def pred(self,x):
+    def pred(self, x):
         """pred func"""
-        if x[self.feature]>self.threshold:
+        if x[self.feature] >    self.threshold:
             return self.left_child.pred(x)
         else:
             return self.right_child.pred(x)
@@ -205,7 +205,7 @@ class Decision_Tree:
         self.update_bounds()
         leaves = self.get_leaves()
         for leaf in leaves:
-            leaf.update_indicator()     
+            leaf.update_indicator()
         self.predict = lambda A: np.sum(
             np.array([leaf.indicator(A) * leaf.value for leaf in leaves]),
             axis=0
