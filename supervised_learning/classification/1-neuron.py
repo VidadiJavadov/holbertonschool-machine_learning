d11dc9bad8128aecedf1e502b7b3eabc9ac326a0
VidadiJavadov <vidadi4000@gmail.com>
2025-09-01 03:17:11 +0400

    1-neuron.py

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""0-neuron"""
+
+import numpy as np
+
+
+class Neuron:
+    """class which defines neuron"""
+
+    def __init__(self, nx):
+        """initializing function"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
