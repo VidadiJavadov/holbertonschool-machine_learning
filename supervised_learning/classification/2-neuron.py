b4b42b91b1e65de565015b5e4b00a300fbf8e47d
VidadiJavadov <vidadi4000@gmail.com>
2025-09-01 03:32:11 +0400

    2-neuron.py

---
+++ supervised_learning/classification/2-neuron.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""0-neuron"""
+
+import numpy as np
+
+
+class Neuron:
+    """class which defines neuron"""
+
+    def __init__(self, nx):
+        """initializing function"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """getter for Weight"""
+        return self.__W
+
+    @property
+    def b(self):
+        """getter for bias"""
+        return self.__b
+
+    @property
+    def A(self):
+        """getter for Act"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """forward_prop"""
+        X = np.ndarray.shape(nx, m)
+        Z = np.matmul(self.__W, X) + self.b
+        self.__A = 1 / (1 + np.exp(-Z))  # sigmoid activation
+        return self.__A
