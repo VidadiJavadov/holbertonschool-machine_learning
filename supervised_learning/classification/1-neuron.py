9d9ab40d0b821f372f86251eacc919887c374ff0
VidadiJavadov <vidadi4000@gmail.com>
2025-09-01 03:21:25 +0400

    1-neuron.py

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -14,21 +14,21 @@ class Neuron:
         if nx < 1:
             raise ValueError("nx must be a positive integer")
 
-        self.__W = np.random.randn(1, nx)
-        self.__b = 0
-        self.__A = 0
+        self._W = np.random.randn(1, nx)
+        self._b = 0
+        self._A = 0
 
         @property
         def W(self):
             """getter for Weight"""
-            return self.__W
+            return self._W
 
         @property
         def b(self):
             """getter for bias"""
-            return self.__b
+            return self._b
 
         @property
         def A(self):
             """getter for Act"""
-            return self.__A
+            return self._A
