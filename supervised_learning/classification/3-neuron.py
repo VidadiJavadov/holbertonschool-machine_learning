cc38626dacb3aff02335a9cf86a7b2a647af01d5
VidadiJavadov <vidadi4000@gmail.com>
2025-09-01 17:14:17 +0400

    3-neuron.py

--- supervised_learning/classification/3-neuron.py
+++ supervised_learning/classification/3-neuron.py
@@ -42,5 +42,5 @@ class Neuron:
     def cost(self, Y, A):
         """cost function"""
         m = Y.shape[1]
-        cost = -(1/m) * (np.sum(Y * np.log(A)) + (1-Y) * np.log(1.0000001-A))
+        cost = -(1/m) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
         return cost
