8ddea0eef48d8cb5acf7bd7a2437631afe0ddb01
VidadiJavadov <vidadi4000@gmail.com>
2025-09-03 03:23:44 +0400

    18-deep_neural_network.py

--- supervised_learning/classification/18-deep_neural_network.py
+++ supervised_learning/classification/18-deep_neural_network.py
@@ -4,36 +4,46 @@ import numpy as np
 
 
 class DeepNeuralNetwork:
+    """Deep Neural Network class."""
+
     def __init__(self, nx, layers):
+        """Init Function"""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list) or len(layers) == 0 or not all(isinstance(l, int) and l > 0 for l in layers):
+        if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
 
         self.__L = len(layers)
         self.__cache = {}
         self.__weights = {}
 
-        # He initialization
         for i in range(self.__L):
-            if i == 0:
-                self.__weights["W1"] = np.random.randn(layers[i], nx) * np.sqrt(2 / nx)
-            else:
-                self.__weights[f"W{i+1}"] = np.random.randn(layers[i], layers[i-1]) * np.sqrt(2 / layers[i-1])
-            self.__weights[f"b{i+1}"] = np.zeros((layers[i], 1))
+            if not isinstance(layers[i], int) or layers[i] <= 0:
+                raise TypeError("layers must be a list of positive integers")
 
-    @property
-    def cache(self):
-        return self.__cache
+            nodes = layers[i]
+            prev_nodes = nx if i == 0 else layers[i - 1]
+
+            self.__weights["W{}".format(i + 1)] = (
+                np.random.randn(nodes, prev_nodes) * np.sqrt(2 / prev_nodes)
+            )
+            self.__weights["b{}".format(i + 1)] = np.zeros((nodes, 1))
 
     @property
     def L(self):
+        """Getter for number of layers."""
         return self.__L
 
     @property
+    def cache(self):
+        """Getter for cache dictionary."""
+        return self.__cache
+
+    @property
     def weights(self):
+        """Getter for weights dictionary."""
         return self.__weights
 
     def forward_prop(self, X):
