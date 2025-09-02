d392e9cbd10fc1991d736199e36872cebeaee683
VidadiJavadov <vidadi4000@gmail.com>
2025-09-02 17:44:51 +0400

    13-neural_network.py

--- supervised_learning/classification/13-neural_network.py
+++ supervised_learning/classification/13-neural_network.py
@@ -81,16 +81,16 @@ class NeuralNetwork:
         """gradient_descent"""
         m = X.shape[1]
 
-        dZ1 = A1 - Y
+        dZ2 = A2 - Y
+        dW2 = (1 / m) * np.dot(dZ2, A1.T)
+        db2 = (1 / m) * np.sum(dZ2)
+
+        self.__W2 -= alpha * dW2
+        self.__b2 -= alpha * db2
+
+        dZ1 = np.dot(self.__W2.T, dZ2) * A1 * (1 - A1)
         dW1 = (1 / m) * np.dot(dZ1, X.T)
         db1 = (1 / m) * np.sum(dZ1)
 
         self.__W1 -= alpha * dW1
         self.__b1 -= alpha * db1
-
-        dZ2 = A2 - Y
-        dW2 = (1 / m) * np.dot(dZ2, X.T)
-        db2 = (1 / m) * np.sum(dZ2)
-
-        self.__W2 -= alpha * dW2
-        self.__b2 -= alpha * db2
