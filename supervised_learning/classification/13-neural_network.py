393461db6e5c85a6ef1f985bc2de624d0cd538ca
VidadiJavadov <vidadi4000@gmail.com>
2025-09-02 04:52:44 +0400

    13-neural_network.py

---
+++ supervised_learning/classification/13-neural_network.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+"""NeuralNetwork class"""
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """neuralnetwork"""
+    def __init__(self, nx, nodes):
+        """init func"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """getter for W1"""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """getter for b1"""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """getter for A1"""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """getter for W2"""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """getter for b2"""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """getter for A2"""
+        return self.__A2
+
+    def forward_prop(self, X):
+        """forward_prop"""
+        Z1 = np.matmul(self.__W1, X) + self.__b1
+        self.__A1 = 1/(1 + np.exp(-Z1))
+        Z2 = np.matmul(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1/(1 + np.exp(-Z2))
+
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        """cost func"""
+        m = Y.shape[1]
+        cost = -(1/m) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        return cost
+
+    def evaluate(self, X, Y):
+        """evaluation"""
+        _, A2 = self.forward_prop(X)
+        cost = self.cost(Y, A2)
+
+        return np.where(A2 >= 0.5, 1, 0), cost
+
+    def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
+        """gradient_descent"""
+        m = X.shape[1]
+
+        dZ1 = A1 - Y
+        dW1 = (1 / m) * np.dot(dZ1, X.T)
+        db1 = (1 / m) * np.sum(dZ1)
+
+        self.__W1 -= alpha * dW1
+        self.__b1 -= alpha * db1
+
+        dZ2 = A2 - Y
+        dW2 = (1 / m) * np.dot(dZ2, X.T)
+        db2 = (1 / m) * np.sum(dZ2)
+
+        self.__W2 -= alpha * dW2
+        self.__b2 -= alpha * db2
