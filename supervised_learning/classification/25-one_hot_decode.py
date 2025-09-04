07b926992b7fe8e30527d816aeb8ac83aa44b84e
VidadiJavadov <vidadi4000@gmail.com>
2025-09-04 17:30:03 +0400

    all

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -5,4 +5,6 @@ import numpy as np
 
 def one_hot_decode(one_hot):
     """one hot decode"""
+    if isinstance(one_hot, np.ndarray) or one_hot.ndim != 2:
+        return None
     return np.argmax(one_hot)
