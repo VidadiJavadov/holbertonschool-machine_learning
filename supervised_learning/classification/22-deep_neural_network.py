7046ab8ffcf6f719ef02749e0a70d7fb9d82b651
VidadiJavadov <vidadi4000@gmail.com>
2025-09-04 06:24:39 +0400

    all

--- supervised_learning/classification/22-deep_neural_network.py
+++ supervised_learning/classification/22-deep_neural_network.py
@@ -109,7 +109,7 @@ class DeepNeuralNetwork:
             raise TypeError("alpha must be a float")
         if alpha <= 0:
             raise ValueError("alpha must be positive")
-        
+
         for i in range(iterations):
             self.forward_prop(X)
             self.gradient_descent(Y, self.__cache, alpha)
