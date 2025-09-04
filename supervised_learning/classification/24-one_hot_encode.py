f3705bfc93c883f5bb39156e3d69d392fcdf52f2
VidadiJavadov <vidadi4000@gmail.com>
2025-09-04 17:25:12 +0400

    all

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -4,7 +4,10 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """one hot encode"""
-    m = Y.shape[0]
-    ohe_mat = np.zeros((classes, m))
-    ohe_mat[Y, np.arange(m)] = 1
-    return ohe_mat
+    try:
+        m = Y.shape[0]
+        ohe_mat = np.zeros((classes, m))
+        ohe_mat[Y, np.arange(m)] = 1
+        return ohe_mat
+    except:
+        return None
