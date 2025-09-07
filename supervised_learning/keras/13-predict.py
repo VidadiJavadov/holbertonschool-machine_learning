713e247013b0cf45b9e839c183295e9c53abfbf0
VidadiJavadov <vidadi4000@gmail.com>
2025-09-07 14:57:36 +0400

    all

--- supervised_learning/keras/13-predict.py
+++ supervised_learning/keras/13-predict.py
@@ -3,6 +3,7 @@
 
 import tensorflow.keras as K
 
+
 def predict(network, data, verbose=False):
     """Makes a prediction using a neural network."""
     return network.predict(x=data, verbose=verbose)
