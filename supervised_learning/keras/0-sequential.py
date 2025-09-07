8eb336ede5c256655de5c30234660a52a6f4ff9e
VidadiJavadov <vidadi4000@gmail.com>
2025-09-07 04:54:41 +0400

    all

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -16,14 +16,17 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
         kernel_regularizer=K.regularizers.l2(lambtha),
         input_dim=nx
     ))
+    if len(layers) > 1 and keep_prob < 1:
+        model.add(K.layers.Dropout(1 - keep_prob))
 
-    # Add remaining layers with dropout (except output layer)
+    # Remaining hidden/output layers
     for i in range(1, len(layers)):
         model.add(K.layers.Dense(
             units=layers[i],
             activation=activations[i],
             kernel_regularizer=K.regularizers.l2(lambtha)
         ))
+        # Add dropout only if it's not the output layer
         if i != len(layers) - 1 and keep_prob < 1:
             model.add(K.layers.Dropout(1 - keep_prob))
 
