6f733e839cc5bd5cdc819977382fbe1e1cec48ca
VidadiJavadov <vidadi4000@gmail.com>
2025-09-07 14:44:20 +0400

    all

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -3,6 +3,7 @@
 
 import tensorflow.keras as K
 
+
 def train_model(network, data, labels, batch_size, epochs,
                 verbose=True, shuffle=False):
     """Trains a model using mini-batch gradient descent."""
