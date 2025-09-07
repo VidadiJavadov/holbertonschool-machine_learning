989a1fd8a565283f6b6534c60bc94a345fd0fda3
VidadiJavadov <vidadi4000@gmail.com>
2025-09-07 14:43:06 +0400

    all

--- supervised_learning/keras/3-one_hot.py
+++ supervised_learning/keras/3-one_hot.py
@@ -3,6 +3,7 @@
 
 import tensorflow.keras as K
 
+
 def one_hot(labels, classes=None):
     """Converts a label vector into a one-hot encoded matrix."""
     return K.utils.to_categorical(labels, num_classes=classes)
