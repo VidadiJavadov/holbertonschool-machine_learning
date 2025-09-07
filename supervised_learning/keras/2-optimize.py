710fd7675c0c38cbc135ebdd07290cda09a71bdb
VidadiJavadov <vidadi4000@gmail.com>
2025-09-07 14:38:32 +0400

    all

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -2,9 +2,10 @@
 """optimize"""
 import tensorflow.keras as K
 
+
 def optimize_model(network, alpha, beta1, beta2):
     """Configures Adam optimizer with categorical crossentropy and accuracy."""
-    optimizer = K.optimizers.Adam(learning_rate=alpha, beta_1=beta1, beta_2=beta2)
-    network.compile(optimizer=optimizer,
+    op = K.optimizers.Adam(learning_rate=alpha, beta_1=beta1, beta_2=beta2)
+    network.compile(optimizer=op,
                     loss='categorical_crossentropy',
                     metrics=['accuracy'])
