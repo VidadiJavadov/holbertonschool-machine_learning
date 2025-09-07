49e6d96f40db57b370bc1bc6ee00578ef47d7ddc
VidadiJavadov <vidadi4000@gmail.com>
2025-09-07 14:46:18 +0400

    all

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -6,7 +6,7 @@ import tensorflow.keras as K
 
 def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, verbose=True, shuffle=False):
-    """Trains a model using mini-batch gradient descent and optional validation."""
+    """Trains a model using mini-baescent and optional validation."""
     return network.fit(x=data,
                        y=labels,
                        batch_size=batch_size,
