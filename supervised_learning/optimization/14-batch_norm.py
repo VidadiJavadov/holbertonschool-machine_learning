8dc05b48dd43689b71f8b02aaae7a7c033897349
VidadiJavadov <vidadi4000@gmail.com>
2025-09-13 03:58:52 +0400

    all

---
+++ supervised_learning/optimization/14-batch_norm.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Batch normalization with tensorflow"""
+import keras as tf
+
+
+def create_batch_norm_layer(prev, n, activation):
+    """batch norm with tensorflow"""
+
+    dense = tf.layers.Dense(
+        n,
+        kernel_initializer=tf.keras.initializers.VarianceScaling(mode='fan_avg'),
+        use_bias=False
+    )(prev)
+
+    bn = tf.layers.BatchNormalization(
+        axis=-1,
+        momentum=0.99,
+        epsilon=1e-7,
+        center=True,
+        scale=True   
+    )(dense)
+
+    output = activation(bn)
+    return output
