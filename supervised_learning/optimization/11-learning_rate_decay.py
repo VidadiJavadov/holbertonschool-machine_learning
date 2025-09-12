55b1c389e74700ae495a81e6f0668cd9a707d531
VidadiJavadov <vidadi4000@gmail.com>
2025-09-13 00:17:16 +0400

    all

--- supervised_learning/optimization/11-learning_rate_decay.py
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -5,5 +5,6 @@ import numpy as np
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """learning rate decay"""
-    alpha = alpha * decay_rate**(global_step/decay_step)
-    return alpha
+    step = np.floor(global_step/decay_step)
+    new_alpha = alpha / (1+ decay_rate * step)
+    return new_alpha
