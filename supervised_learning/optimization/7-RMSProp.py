6c7d10173e0fc886faff3e3faf4e023db6799fd5
VidadiJavadov <vidadi4000@gmail.com>
2025-09-12 19:34:44 +0400

    all

--- supervised_learning/optimization/7-RMSProp.py
+++ supervised_learning/optimization/7-RMSProp.py
@@ -7,4 +7,4 @@ def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     """RMSProp"""
     s = beta2 * s + (1 - beta2) * grad**2
     var = var - alpha * (grad/np.sqrt(s) + epsilon)
-
+    return var, s
