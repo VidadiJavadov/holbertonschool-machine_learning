f9eca04b9f380c0563301db2b3fc5e8da829e250
VidadiJavadov <vidadi4000@gmail.com>
2025-09-17 19:30:27 +0400

    all

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -2,12 +2,13 @@
 """L2 Regularization Cost"""
 import numpy as np
 
+
 def l2_reg_cost(cost, lambtha, weights, L, m):
     """function that computes l2_reg"""
     l2_sum = 0
-    for i in range (1, L+1):
+    for i in range(1, L+1):
         W = weights["W"+str(i)]
         l2_sum += np.sum(np.square(W))
 
-    l2_reg = cost + (lambtha / (2 * m))* l2_sum
+    l2_reg = cost + (lambtha / (2 * m)) * l2_sum
     return l2_reg
