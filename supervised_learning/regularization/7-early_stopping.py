5a1b317c5d7f0e8ea0ab3c72917a369d10502329
VidadiJavadov <vidadi4000@gmail.com>
2025-09-20 03:09:24 +0400

    all

--- supervised_learning/regularization/7-early_stopping.py
+++ supervised_learning/regularization/7-early_stopping.py
@@ -13,8 +13,3 @@ def early_stopping(cost, opt_cost, threshold, patience, count):
 
     stop = count >= patience
     return stop, count
-
-print(early_stopping(1.0, 1.9, 0.5, 15, 5))
-print(early_stopping(1.1, 1.5, 0.5, 15, 2))
-print(early_stopping(1.0, 1.5, 0.5, 15, 8))
-print(early_stopping(1.0, 1.5, 0.5, 15, 14))
