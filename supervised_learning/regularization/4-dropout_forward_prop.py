ba45806f2e5d5436c5613f988cd1a9ae97cdeba8
VidadiJavadov <vidadi4000@gmail.com>
2025-09-20 02:07:47 +0400

    all

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -15,15 +15,15 @@ def dropout_forward_prop(X, weights, L, keep_prob):
 
         Z = np.matmul(W, A_prev) + b
 
-        if i<L:
+        if i < L:
             A = np.tanh(Z)
             D = np.random.rand(*A.shape) < keep_prob
             cache["D" + str(i)] = D.astype(int)
             A = (A * D) / keep_prob
         else:
             exp_Z = np.exp(Z - np.max(Z, axis=0, keepdims=True))
-            A = exp_Z / np.sum(exp_Z, axis=0, keepdims=True) 
-        
+            A = exp_Z / np.sum(exp_Z, axis=0, keepdims=True)
+
         cache["A" + str(i)] = A
 
     return cache
