a491ea03e5a022fd032fecdf33c532ad55933c18
VidadiJavadov <vidadi4000@gmail.com>
2025-09-20 03:14:32 +0400

    all

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -3,40 +3,23 @@
 import numpy as np
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
-    """
-    Updates the weights and biases of a neural network using gradient descent
-    with L2 regularization.
-
-    Args:
-        Y (np.ndarray): One-hot labels of shape (classes, m)
-        weights (dict): Dictionary of weights and biases
-        cache (dict): Dictionary of layer outputs
-        alpha (float): Learning rate
-        lambtha (float): L2 regularization parameter
-        L (int): Number of layers
-
-    Returns:
-        None (weights and biases updated in place)
-    """
+    """gradient descent with l2"""
     m = Y.shape[1]
-    # Initialize dZ for last layer (softmax)
-    A_prev = cache["A" + str(L - 1)]
-    A_L = cache["A" + str(L)]
-    
-    dZ = A_L - Y  # derivative of softmax cross-entropy
+    dZ = cache["A" + str(L)] - Y  # derivative of cost wrt Z for softmax output
+
     for l in reversed(range(1, L + 1)):
         A_prev = cache["A" + str(l - 1)]
         W = weights["W" + str(l)]
-        
-        # Compute gradients with L2 regularization
-        dW = (np.dot(dZ, A_prev.T) / m) + (lambtha / m) * W
-        db = np.sum(dZ, axis=1, keepdims=True) / m
-        
-        # Update weights and biases
+
+        # gradients with L2 regularization
+        dW = (1 / m) * np.matmul(dZ, A_prev.T) + (lambtha / m) * W
+        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+
+        # update weights and biases
         weights["W" + str(l)] -= alpha * dW
         weights["b" + str(l)] -= alpha * db
-        
+
         if l > 1:
-            # Backpropagate through tanh activation
-            A_prev = cache["A" + str(l - 1)]
-            dZ = np.dot(W.T, dZ) * (1 - A_prev ** 2)
+            # propagate dZ backward using derivative of tanh
+            dA_prev = np.matmul(W.T, dZ)
+            dZ = dA_prev * (1 - cache["A" + str(l - 1)] ** 2)
