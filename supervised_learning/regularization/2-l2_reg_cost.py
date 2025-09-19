1dcad4257924f03b611d48ee1746204ae52ac8a2
VidadiJavadov <vidadi4000@gmail.com>
2025-09-19 22:01:52 +0400

    all

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -7,5 +7,5 @@ def l2_reg_cost(cost, model):
     """function that computes L2 reg"""
 
     l2_loss = model.losses
-    total_cost = [cost + l for l in l2_loss]
+    total_cost = [cost + L for L in l2_loss]
     return tf.convert_to_tensor(total_cost)
