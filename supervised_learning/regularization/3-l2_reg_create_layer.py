427ebdb1be30e3b3e806754beb455e67859c90c5
VidadiJavadov <vidadi4000@gmail.com>
2025-09-19 22:16:40 +0400

    all

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """Creating a layer with l2 reg"""
-import keras as tf
+import tensorflow as tf
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """Creating a layer with l2 reg"""
-    l2_reg = tf.regularizers.l2(lambtha)
+    l2_reg = tf.keras.regularizers.l2(lambtha)
 
-    layer = tf.layers.Dense(
+    layer = tf.keras.layers.Dense(
         units=n,
         activation=activation,
         kernel_regularizer=l2_reg
