2c1da9f14786be132af5807e2a7e3a1174341739
VidadiJavadov <vidadi4000@gmail.com>
2025-09-19 21:54:43 +0400

    all

---
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""L2 reg for grad"""
+import numpy as np
+
+
+def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
+    """l2 reg"""
+    
---
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""L2 reg cost"""
+import tensorflow as tf
+
+
+def l2_reg_cost(cost, model):
+    """function that computes L2 reg"""
+
+    l2_loss = tf.add_n(model.losses)
+    return cost + l2_loss
