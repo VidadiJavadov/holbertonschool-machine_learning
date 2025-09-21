71924c7d5060ffe4794dae77d22c24a0e26822f1
VidadiJavadov <vidadi4000@gmail.com>
2025-09-21 19:23:15 +0400

    all

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -1,25 +1,21 @@
 #!/usr/bin/env python3
-"""L2 regularized gradient descent"""
+"""Comment of Function"""
 import numpy as np
 
+
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
-    """gradient descent with l2"""
+    """L2 Regularization Gradient Descent"""
+    AL = cache['A{}'.format(L)]
+    dZ = AL - Y
     m = Y.shape[1]
-    dZ = cache["A" + str(L)] - Y  # derivative of cost wrt Z for softmax output
-
-    for l in reversed(range(1, L + 1)):
-        A_prev = cache["A" + str(l - 1)]
-        W = weights["W" + str(l)]
-
-        # gradients with L2 regularization
-        dW = (1 / m) * np.matmul(dZ, A_prev.T) + (lambtha / m) * W
+    for i in range(L, 0, -1):
+        W = weights['W{}'.format(i)]
+        A_prev = cache['A{}'.format(i - 1)]
+        dW = (1/m) * (dZ @ A_prev.T) + (lambtha/m) * W
         db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
-
-        # update weights and biases
-        weights["W" + str(l)] -= alpha * dW
-        weights["b" + str(l)] -= alpha * db
-
-        if l > 1:
-            # propagate dZ backward using derivative of tanh
-            dA_prev = np.matmul(W.T, dZ)
-            dZ = dA_prev * (1 - cache["A" + str(l - 1)] ** 2)
+        if i > 1:
+            dZ_prev = (W.T @ dZ) * (1 - A_prev ** 2)
+        weights['W{}'.format(i)] -= alpha * dW
+        weights['b{}'.format(i)] -= alpha * db
+        if i > 1:
+            dZ = dZ_prev
