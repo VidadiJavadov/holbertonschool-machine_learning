47a3cc55cae435dab3e8e866b116614f26cf39a7
VidadiJavadov <vidadi4000@gmail.com>
2025-09-21 14:03:32 +0400

    all

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -7,7 +7,7 @@ def create_confusion_matrix(labels, logits):
     """conf mat"""
     true_labels = np.argmax(labels, axis=1)
     true_logits = np.argmax(logits, axis=1)
-    classes = true_labels.shape[1]
+    classes = labels.shape[1]
     cm = np.zeros((classes, classes), dtype=int)
     for i, j in zip(true_labels, true_logits):
         cm[i][j] += 1
