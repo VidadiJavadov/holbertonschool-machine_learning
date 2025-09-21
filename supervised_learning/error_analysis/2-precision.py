5269e90d85e7e5e60c17df3854033cf164fb5880
VidadiJavadov <vidadi4000@gmail.com>
2025-09-21 18:03:28 +0400

    all

--- supervised_learning/error_analysis/2-precision.py
+++ supervised_learning/error_analysis/2-precision.py
@@ -6,7 +6,7 @@ import numpy as np
 def precision(confusion):
     """precision"""
     precision = []
-    for i in range(confusion):
+    for i in range(confusion[0]):
         TP = confusion[i, i]
         FP = np.sum(confusion[:, i]) - TP
         prec = TP / (TP + FP) if (TP + FP) > 0 else 0
