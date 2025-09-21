fec5569db873b0839e8d2b7945d07c856d8bf98d
VidadiJavadov <vidadi4000@gmail.com>
2025-09-21 17:11:28 +0400

    all

--- supervised_learning/error_analysis/1-sensitivity.py
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -6,7 +6,7 @@ import numpy as np
 def sensitivity(confusion):
     """sensitivity"""
     sensitivity = []
-    for i in range(len(confusion[0])):
+    for i in range(len(confusion)):
         TP = confusion[i, i]
         FN = np.sum(confusion[i, :]) - TP
         sens = TP / (TP + FN)
