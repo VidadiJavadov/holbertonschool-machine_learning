71c65085c6e1b31e66253f8fd92bc5a58858a40f
VidadiJavadov <vidadi4000@gmail.com>
2025-09-28 22:37:01 +0400

    4-convolve_channels.py

---
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""convolve multi-channel images"""
+import numpy as np
+
+
+def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
+    """performs convolution on images with channels"""
+    m, h, w, c = images.shape
+    kh, kw, kc = kernel.shape
+    sh, sw = stride
+
+    if kc != c:
+        raise ValueError("Kernel channels must match image channels")
+
+    if type(padding) == tuple:
+        ph, pw = padding
+    elif padding == 'valid':
+        ph, pw = 0, 0
+    elif padding == 'same':
+        ph = max((int(np.ceil(((h - 1) * sh + kh - h) / 2))), 0)
+        pw = max((int(np.ceil(((w - 1) * sw + kw - w) / 2))), 0)
+    else:
+        raise ValueError("padding must be 'same', 'valid', or a tuple")
+
+    padded = np.pad(images,
+                    ((0, 0), (ph, ph), (pw, pw), (0, 0)),
+                    mode='constant')
+
+    out_h = ((h + 2 * ph - kh) // sh) + 1
+    out_w = ((w + 2 * pw - kw) // sw) + 1
+
+    output = np.zeros((m, out_h, out_w))
+
+    for i in range(out_h):
+        for j in range(out_w):
+            region = padded[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :]
+            output[:, i, j] = np.sum(region * kernel, axis=(1, 2, 3))
+
+    return output
