678df20838dbca271f3e7b5e1f16c70f192cda21
VidadiJavadov <vidadi4000@gmail.com>
2025-09-28 22:38:53 +0400

    git p

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -9,9 +9,8 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     kh, kw = kernel.shape
     sh, sw = stride
 
-    if isinstance(padding) == tuple:
-        ph, pw = padding
-    elif padding == 'valid':
+    ph, pw = padding
+    if padding == 'valid':
         ph, pw = 0, 0
     elif padding == 'same':
         ph = max((int(np.ceil(((h - 1) * sh + kh - h) / 2))), 0)
