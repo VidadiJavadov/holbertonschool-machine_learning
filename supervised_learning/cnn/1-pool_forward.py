dfb2e4e16794e059ca0bf5d8e067e42e8f227677
VidadiJavadov <vidadi4000@gmail.com>
2025-10-05 20:04:28 +0400

    1-pool_forward.py

--- supervised_learning/cnn/1-pool_forward.py
+++ supervised_learning/cnn/1-pool_forward.py
@@ -18,7 +18,8 @@ def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
             for w in range(w_new):
                 vert_start, vert_end = h * sh, h * sh + kh
                 horiz_start, horiz_end = w * sw, w * sw + kw
-                A_slice = A_prev[i, vert_start:vert_end, horiz_start:horiz_end, :]
+                A_slice = A_prev[i, vert_start:vert_end, 
+                                 horiz_start:horiz_end, :]
 
                 if mode == 'max':
                     A[i, h, w, :] = np.max(A_slice, axis=(0, 1))
