9770c4b168592814a226f506fe0a69ed7cfedd0a
VidadiJavadov <vidadi4000@gmail.com>
2025-11-25 19:27:39 +0400

    0-likelihood.py

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
-import numpy as np
 """Likelihood probability"""
+
+import numpy as np
 
 
 def likelihood(x, n, P):
-    """Calculate the likelihood of observing x 
-    severe side effects out of n patients."""
-
+    """
+    Calculate the likelihood of observing x severe side effects
+    out of n patients.
+    """
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
 
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater " \
-        "than or equal to 0")
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0"
+        )
 
     if x > n:
         raise ValueError("x cannot be greater than n")
@@ -21,9 +24,12 @@ def likelihood(x, n, P):
         raise TypeError("P must be a 1D numpy.ndarray")
 
     if np.any((P < 0) | (P > 1)):
-        raise ValueError("All values in P must be in the range [0, 1]")
+        raise ValueError(
+            "All values in P must be in the range [0, 1]"
+        )
 
-    coef = np.math.factorial(n) / (np.math.factorial(n-x) * np.math.factorial(x))
+    coef = (np.math.factorial(n) /
+            (np.math.factorial(n - x) * np.math.factorial(x)))
 
     likelihoods = coef * (P ** x) * ((1 - P) ** (n - x))
 
