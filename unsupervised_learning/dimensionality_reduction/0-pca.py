e8c834d4007e2592d12bd294287207213f8de626
VidadiJavadov <vidadi4000@gmail.com>
2025-11-30 20:30:39 +0400

    0-pca.py

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -1,12 +1,9 @@
-#!/usr/bin/env python3
 import numpy as np
 
-
 def pca(X, var=0.95):
-    """Perform PCA on X while preserving var fraction of variance."""
-    U, S, Vt = np.linalg.svd(X, full_matrices=False)
-    eig_vals = (S ** 2) / (X.shape[0] - 1)
-    eig_vecs = Vt.T
+    """Perform PCA on X preserving var fraction of variance."""
+    cov = np.cov(X, rowvar=False)
+    eig_vals, eig_vecs = np.linalg.eigh(cov)
 
     idx = np.argsort(eig_vals)[::-1]
     eig_vals = eig_vals[idx]
@@ -16,6 +13,4 @@ def pca(X, var=0.95):
     cumulative = np.cumsum(explained)
 
     nd = np.searchsorted(cumulative, var) + 1
-    nd = min(nd, eig_vecs.shape[1])
-
     return eig_vecs[:, :nd]
