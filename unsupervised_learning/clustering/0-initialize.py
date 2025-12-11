886d874ffb6ef158653bc4c53794e4a594e694ae
VidadiJavadov <vidadi4000@gmail.com>
2025-12-11 15:48:09 +0400

    0-initialize.py

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python3
+"""Initialize K-means centroids."""
+
 import numpy as np
-"""initialize k-means"""
 
 
 def initialize(X, k):
-    """Initializes K-means centroids with a multivariate uniform distrib."""
+    """Initialize K-means centroids uniformly within data bounds."""
     if (
-    not isinstance(X, np.ndarray)
-    or X.ndim != 2
-    or not isinstance(k, int)
-    or k <= 0
+        not isinstance(X, np.ndarray)
+        or X.ndim != 2
+        or not isinstance(k, int)
+        or k <= 0
     ):
         return None
 
-    # Minimum and maximum values per dimension
     min_vals = X.min(axis=0)
     max_vals = X.max(axis=0)
 
-    # Initialize centroids uniformly between min_vals and max_vals
     centroids = np.random.uniform(min_vals, max_vals, (k, X.shape[1]))
 
     return centroids
