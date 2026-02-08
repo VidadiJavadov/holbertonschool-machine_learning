e79eb1ecbf3235ba6a25d95ef2c3eacfb05b50fd
VidadiJavadov <vidadi4000@gmail.com>
2026-02-08 21:45:48 +0400

    2-rnn_decoder.py

--- supervised_learning/attention/2-rnn_decoder.py
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -1,52 +1,53 @@
 #!/usr/bin/env python3
+"""
+RNNDecoder for Machine Translation
+"""
 import tensorflow as tf
-
 SelfAttention = __import__('1-self_attention').SelfAttention
 
 
 class RNNDecoder(tf.keras.layers.Layer):
-    """RNN Decoder with attention for machine translation"""
+    """
+    Decoder class that inherits from tensorflow.keras.layers.Layer
+    to decode for machine translation
+    """
 
     def __init__(self, vocab, embedding, units, batch):
-        """Initialize decoder"""
+        """
+        Class constructor
+        """
         super(RNNDecoder, self).__init__()
-
-        self.embedding = tf.keras.layers.Embedding(
-            input_dim=vocab,
-            output_dim=embedding
-        )
-
-        self.gru = tf.keras.layers.GRU(
-            units,
-            return_sequences=True,
-            return_state=True,
-            recurrent_initializer='glorot_uniform'
-        )
-
+        self.embedding = tf.keras.layers.Embedding(vocab, embedding)
+        self.gru = tf.keras.layers.GRU(units,
+                                       return_sequences=True,
+                                       return_state=True,
+                                       recurrent_initializer='glorot_uniform')
         self.F = tf.keras.layers.Dense(vocab)
         self.attention = SelfAttention(units)
 
     def call(self, x, s_prev, hidden_states):
         """
-        Perform one decoding step
+        Performs the decoding step
         """
-        # x: (batch, 1)
-        x = self.embedding(x)  # (batch, 1, embedding)
+        # Get context vector and attention weights
+        context_vector, _ = self.attention(s_prev, hidden_states)
 
-        # Attention
-        context, _ = self.attention(s_prev, hidden_states)
-        context = tf.expand_dims(context, 1)  # (batch, 1, units)
+        # Embed the input word x: (batch, 1) -> (batch, 1, embedding)
+        x = self.embedding(x)
 
-        # Concatenate context and embedding
-        x = tf.concat([context, x], axis=-1)
+        # Expand context vector: (batch, units) -> (batch, 1, units)
+        context_expanded = tf.expand_dims(context_vector, 1)
 
-        # GRU forward pass
-        outputs, s = self.gru(x, initial_state=s_prev)
+        # Concatenate context vector with x: (batch, 1, units + embedding)
+        x = tf.concat([context_expanded, x], axis=-1)
 
-        # Use last time step
-        outputs = outputs[:, -1, :]  # (batch, units)
+        # Pass through GRU
+        output, s = self.gru(x)
 
-        # Final prediction
-        y = self.F(outputs)  # (batch, vocab)
+        # Reshape output: (batch, 1, units) -> (batch, units)
+        output = tf.reshape(output, (-1, output.shape[2]))
+
+        # Pass through Dense layer to get output logits
+        y = self.F(output)
 
         return y, s
