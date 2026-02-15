3e56e3eb634056c1d8cea86f9d81e2698258690d
VidadiJavadov <vidadi4000@gmail.com>
2026-02-15 17:06:07 +0400

    @all@

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -3,8 +3,7 @@
 Module to load and prep a dataset for machine translation.
 """
 import tensorflow_datasets as tfds
-import tensorflow as tf
-from transformers import AutoTokenizer
+import transformers
 
 class Dataset:
     """
@@ -22,34 +21,41 @@ class Dataset:
             as_supervised=True
         )
 
-        # Build tokenizers based on training data
+        # Build tokenizers from the training set
         self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(self.data_train)
 
     def tokenize_dataset(self, data):
         """
-        Trains sub-word tokenizers from pre-trained BERT models.
+        Trains sub-word tokenizers using BERT base models.
         
         Args:
-            data: tf.data.Dataset (pt, en)
+            data: tf.data.Dataset containing (pt, en) tuples
             
         Returns:
-            (tokenizer_pt, tokenizer_en): The trained tokenizers
+            tokenizer_pt, tokenizer_en: The trained tokenizers
         """
         # Base BERT models for Pt and En
         pt_path = "neuralmind/bert-base-portuguese-cased"
         en_path = "bert-base-uncased"
         
-        # Load configurations
-        tk_pt = AutoTokenizer.from_pretrained(pt_path)
-        tk_en = AutoTokenizer.from_pretrained(en_path)
+        # Load pre-trained configurations
+        tk_pt = transformers.AutoTokenizer.from_pretrained(pt_path)
+        tk_en = transformers.AutoTokenizer.from_pretrained(en_path)
 
         def get_corpus(index):
-            """Generator to yield decoded strings from the dataset."""
-            for pt, en in data:
-                yield pt.numpy().decode('utf-8') if index == 0 else en.numpy().decode('utf-8')
+            """Generator to yield strings from the dataset."""
+            for example in data:
+                # index 0 is Portuguese, index 1 is English
+                yield example[index].numpy().decode('utf-8')
 
-        # Train with vocab limit of 2**13 (8192)
-        self.tokenizer_pt = tk_pt.train_new_from_iterator(get_corpus(0), vocab_size=2**13)
-        self.tokenizer_en = tk_en.train_new_from_iterator(get_corpus(1), vocab_size=2**13)
+        # Train new tokenizers with a vocab limit of 2**13 (8192)
+        tokenizer_pt = tk_pt.train_new_from_iterator(
+            get_corpus(0), 
+            vocab_size=2**13
+        )
+        tokenizer_en = tk_en.train_new_from_iterator(
+            get_corpus(1), 
+            vocab_size=2**13
+        )
 
-        return self.tokenizer_pt, self.tokenizer_en
+        return tokenizer_pt, tokenizer_en
